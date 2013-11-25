14abed6ac3b3e8bc6ebabc978baa99a8f9dd872b
khe2 <khe2@mywebclass.org>
2013-11-25 04:44:05 +0000

    second commit

--- public/layouts/footer.php
+++ public/layouts/footer.php
@@ -1,5 +1,5 @@
     </div>
-    <div id="footer">Copyright <?php echo date("Y", time()); ?>, Kevin me</div>
+    <div id="footer">Copyright <?php echo date("Y", time()); ?>, Khaled Elagamy</div>
   </body>
 </html>
 <?php if(isset($database)) { $database->close_connection(); } ?>
